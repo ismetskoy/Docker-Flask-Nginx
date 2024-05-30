bf56f4aac1e3ae650f39c2728468da25c69c1371
ismetskoy <felhat@bk.ru>
2024-05-30 20:59:40 +0300

    up

--- app/app.py
+++ app/app.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, request
 import requests
-from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
-app.config['MAIL_SERVER'] = 'smtp.mail.ru'
-app.config['MAIL_PORT'] = 465  
-app.config['MAIL_USE_SSL'] = True  
-app.config['MAIL_USERNAME'] = 'felhat.test@bk.ru'
-app.config['MAIL_PASSWORD'] = 'EnzHe7ZbM3hiEY9a6ap9'
 
-mail = Mail(app)
 
 def check_website(url):
     try:
@@ -30,24 +24,6 @@ def monitor():
         status = check_website(url)
         status_list.append((url, status))
     return render_template('index.html', status_list=status_list)
-
-def send_email(subject, sender, recipients, body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = body
-    mail.send(msg)
-
-@app.route('/send_email', methods=['POST'])
-def send_email_route():
-    subject = request.form['subject']
-    sender = request.form['sender']
-    recipients = [request.form['recipients']]
-    body = request.form['body']
-    
-    send_email(subject, sender, recipients, body)
-    
-    back_mail = render_template('email.html', name='Jerry')
-    
-    return back_mail
 
 if __name__ == '__main__':
     #app.debug = True
